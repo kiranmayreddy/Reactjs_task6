14c070042085b9798f466f70e70a3fae0a7daaf1
kiranmayreddy <kiranmayreddy31@gmail.com>
2023-10-14 11:50:50 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+      Hello everyone👋, I'm Kiranmay Reddy, a web development enthusiast and student of data science.
+       I am a third-year student at Vellore Institute of Technology (2021-2025),
+       and I love creating awesome websites.currently, I am learning MongoDB,Node.js and Express.js.
+       I am also familiar with JavaScript and intermediate up to Reactjs,in order to build awesome websites.
+      </p>
+      
+    </section>
+  );
+};
+
+export default About;
